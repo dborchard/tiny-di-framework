441d242c59fa2432336206f1b05194ade4f139dd
Arjun SK <arjun.sunilkumar@nissanmotor.com>
2021-03-26 13:34:57 +0530

    Added TODO

--- codekrypt-di-core/src/main/java/com/arjunsk/codekrypt/di/core/CodekryptInjector.java
+++ codekrypt-di-core/src/main/java/com/arjunsk/codekrypt/di/core/CodekryptInjector.java
@@ -56,6 +56,8 @@ public class CodekryptInjector {
         // Create a bean
         Object beanClassObject = initBeanClass(componentClass, null);
 
+        // TODO: Fix post-construct invocation order.
+
         // Post Construct call.
         ClassObjectUtils.invokePostConstruct(beanClassObject);
       }
